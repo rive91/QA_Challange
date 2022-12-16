ff5eeaf78def652bccdd6a88d385bd1241257474
rive <rive@MSI>
2022-12-15 22:59:25 -0600

    test

--- Sel102022/src/test/java/loginTests/TC02_LoginError.java
+++ Sel102022/src/test/java/loginTests/TC02_LoginError.java
@@ -26,6 +26,7 @@ public class TC02_LoginError {
 	@Test
 	public void TC02() {
 		loginPage.login(GlobalVariables.LOCK_USER, GlobalVariables.ALL_USER_PWD);
+		System.out.println("||||||| Before ASSERT |||||||");
 		Assert.assertTrue(loginPage.validateLockError());
 	}
 	
